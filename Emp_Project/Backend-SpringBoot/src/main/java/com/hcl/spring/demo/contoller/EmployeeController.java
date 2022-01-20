aa8b2c2f31e5a5e070e49789d8cafb1e060d6a69
Pavan-pk98 <98044245+Pavan-pk98@users.noreply.github.com>
2022-01-20 14:56:34 +0530

    Update EmployeeController.java

--- Emp_Project/Backend-SpringBoot/src/main/java/com/hcl/spring/demo/contoller/EmployeeController.java
+++ Emp_Project/Backend-SpringBoot/src/main/java/com/hcl/spring/demo/contoller/EmployeeController.java
@@ -1,7 +1,7 @@
-package com.hcl.spring.demo.contoller;
+package com.spring.demo.contoller;
 
-import com.hcl.spring.demo.models.Employee;
-import com.hcl.spring.demo.services.EmployeeService;
+import com.spring.demo.models.Employee;
+import com.spring.demo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
